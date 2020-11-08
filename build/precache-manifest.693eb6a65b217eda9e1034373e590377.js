6e629d0b4c65bc35a19b658f2f887561ed031985
Gabriel Buchdahl <gabriel@wanderlog.com>
2020-11-08 16:53:17 -0500

    pushing the bui;d

---
+++ build/precache-manifest.693eb6a65b217eda9e1034373e590377.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aeccf68bee537585983cb52a8dbed96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6873d822a2fe9154bcf4",
+    "url": "/static/css/main.cee0d772.chunk.css"
+  },
+  {
+    "revision": "649969e9081845e04cbf",
+    "url": "/static/js/2.03806ebc.chunk.js"
+  },
+  {
+    "revision": "e5067c2807f8c6eeca3663552efd09fb",
+    "url": "/static/js/2.03806ebc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6873d822a2fe9154bcf4",
+    "url": "/static/js/main.d68d5cf2.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
