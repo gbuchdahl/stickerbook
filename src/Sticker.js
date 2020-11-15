b1062778a7a30805bba034b9e75b038f4f22c486
Gabriel Buchdahl <gabriel@wanderlog.com>
2020-11-15 16:52:18 -0500

    consistent glow

--- src/Sticker.js
+++ src/Sticker.js
@@ -5,8 +5,8 @@ import './Sticker.css'
 const emojis = ['😃', '🥳', '🔥', '✨', '😊', '🤩', '🤟', '💅', '💛', '😂', '😏', '😎', '🤠', '😤']
 const Sticker = (props) => {
     let classes = 'emoji';
-    if (props.n % 3 == 0) classes += ' pulse'
-    else if (props.n % 3 == 1) classes += ' pulse4'
+    if (props.n % 3 === 0) classes += ' pulse'
+    else if (props.n % 3 === 1) classes += ' pulse4'
     else classes += ' pulse5'
 
     return <div className='column box is-64x64 is-narrow mx-3'><span role='img' className={classes}>{emojis[Math.floor(Math.random() * emojis.length)]}</span></div>
